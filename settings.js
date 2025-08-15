42ff294a501128243d90665f0427bf9d1f82a9a0
Keith <keithmwai40@gmail.com>
2025-08-15 20:17:48 +0300

    first commit

---
+++ settings.js
@@ -0,0 +1,88 @@
+//-------------------[ BOT SETTINGS ]------------------// 
+
+// @project_name : KANGO-XMD 
+// @author : Hector 
+// @telegram : http://t.me/official_kango
+// @github : OfficialKango
+// @whatsapp : +233509977126
+
+//----------------------[ KANGO-XMD ]----------------------//
+
+const fs = require('fs')
+const { color } = require('./kango/color')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+//--------------------[ SESSION ID ]----------------------//
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+//Enter your KANGO-XMD session id here; must start with KANGO~
+
+//--------------------[ BOT NAME ]----------------------//
+
+global.botname = process.env.BOT_NAME || 'KEITH-XMD' 
+
+//-----------------[ OWNER NUMBER ]------------------//
+
+global.ownernumber = process.env.OWNER_NUMBER || '254741008354' 
+
+//--------------------[ SUDO ]--------------------------//
+
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254741008354', '254741008354'];
+// Type additional allowed users here
+//NB: They'll be able to use every functions of the bot without restrictions.
+
+//-----------------[ OWNER NAME ]------------------//
+
+global.ownername = process.env.OWNER_NAME || 'Keith' 
+
+//------------[ STICKER PACKNAME ]-----------------//
+
+global.packname = process.env.STICKER_PACK_NAME || "KEITH-XMD" 
+
+//--------------[ COUNTRY TIMEZONE ]------------//
+
+
+global.timezones = 'Africa/Accra';  // Set this to you timezone
+
+
+
+//--------------[ STICKER AUTHOR NAME ]------------//
+
+global.author = process.env.STICKER_AUTHOR_NAME || "Keith" 
+
+//----------------[ GITHUB DATABASE ]-----------------//
+
+global.dbToken = process.env.GITHUB_TOKEN || "";
+
+
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "> ©KEITH-XMD"
+
+//---------------------[ REPLIES ]-----------------------//
+
+global.mess = { 
+  done: '*Done*', 
+  success: '©keith-xmd', 
+  owner: `*You don't have permission to use this command!*`, 
+  group: '*This feature becomes available when you use it in a group!*', 
+  admin: '*You’ll unlock this feature with me as an admin!*', 
+  notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
+}
+
+//--------------------[ WATCHER ]-----------------------//
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(color(`Updated '${__filename}'`, 'red'))
+  delete require.cache[file]
+  require(file)
+})
+
+//----------------------[ KANGO-XMD ]----------------------//
